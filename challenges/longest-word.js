c33ec65572384611efa280adf48dd22dbe7eb1c0
livrbecca <ollywalker_1998@outlook.com>
2021-06-01 14:31:20 +0100

    commented out walk through

--- challenges/longest-word.js
+++ challenges/longest-word.js
@@ -1,3 +1,10 @@
+// walk through
+// longest = empty var
+// for loop: initialize i, i less than word length, increment i, so 0 is a number
+// if the length of longest is less than whatever index in the array,
+// set longest to that new index, indicated by i
+
+
 function longestInList(arr) {
   let longest = "";
   for (let i = 0; i < arr.length; i++) {
